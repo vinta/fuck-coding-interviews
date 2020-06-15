0a5e2f9e359d64957dac5b8577a4bff1fb9cf182
Vinta Chen <vinta.chen@gmail.com>
2020-06-16 01:47:14 +0800

    add reverse linked list (recursively)

--- problems/reverse_linked_list.py
+++ problems/reverse_linked_list.py
@@ -1,4 +1,12 @@
 # coding: utf-8
+"""
+https://leetcode.com/problems/reverse-linked-list/
+"""
+import unittest
+
+from utils.linked_list import list_to_listnode
+from utils.linked_list import listnode_to_list
+
 
 # Definition for singly-linked list.
 class ListNode:
@@ -18,3 +26,29 @@ class Solution:
             node = next_node
 
         return previous_node
+
+
+class Solution2:
+    def reverseList(self, head: ListNode) -> ListNode:
+        def reverse(node, previous_node):
+            if not node:
+                return previous_node
+            next_node = node.next
+            node.next = previous_node
+            return reverse(next_node, node)
+
+        return reverse(head, previous_node=None)
+
+
+class TestCase(unittest.TestCase):
+    def setUp(self):
+        self.solution = Solution()
+
+    def test(self):
+        head = list_to_listnode([1, 2, 3, 4, 5])
+        expected_head = self.solution.reverseList(head)
+        expected = [5, 4, 3, 2, 1]
+        self.assertEqual(listnode_to_list(expected_head), expected)
+
+
+unittest.main()
