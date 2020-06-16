4bbddf1950b45af9e7c6bb41e6efdf9a4437bc8c
Vinta Chen <vinta.chen@gmail.com>
2020-06-17 02:00:31 +0800

    refine

--- algorithms/sorting/quick_sort.py
+++ algorithms/sorting/quick_sort.py
@@ -1,15 +1,13 @@
 # coding: utf-8
 """
-Quick Sort is a divide-and-conquer algorithm. It works by
-selecting a pivot element from the array and
-partitioning the other elements into two sub-arrays,
-according to whether they are less than or greater than the pivot.
-The sub-arrays are then sorted recursively.
+Quick Sort
+https://en.wikipedia.org/wiki/Quicksort
 
 Worst-case Complexity: O(n^2)
 Best-case Complexity: O(n * log(n))
 Average Complexity: O(n * log(n))
 """
+import random
 import unittest
 
 
@@ -17,15 +15,36 @@ def quick_sort(arr):
     if len(arr) <= 1:
         return arr
     else:
-        # import random
-        # rand_index = random.randint(0, len(arr) - 1)
-        # pivot = arr.pop(rand_index)  # choose a random item
-        pivot = arr.pop()  # choose the last item
+        # Select the last item as the pivot
+        pivot = arr.pop()
+
+        # Partition the other elements into two sublists,
+        # According to whether they are less than or greater than the pivot
+        left_list = []
+        right_list = []
+        for item in arr:
+            # There might be duplicates of pivot
+            if item <= pivot:
+                left_list.append(item)
+            else:
+                right_list.append(item)
+
+        # The sublists are then sorted recursively
+        return quick_sort(left_list) + [pivot, ] + quick_sort(right_list)
+
+
+def quick_sort2(arr):
+    if len(arr) <= 1:
+        return arr
+    else:
+        # Select a random item as the pivot
+        rand_index = random.randint(0, len(arr) - 1)
+        pivot = arr.pop(rand_index)
 
         left_list = []
         right_list = []
         for item in arr:
-            if item <= pivot:  # there might be duplicates of pivot
+            if item <= pivot:
                 left_list.append(item)
             else:
                 right_list.append(item)
@@ -35,7 +54,7 @@ def quick_sort(arr):
 class TestCase(unittest.TestCase):
     def test(self):
         array = [5, 7, 1, 9, 5, -4, 3, 0, 2]
-        expected = [-4, 0, 1, 2 ,3, 5, 5, 7, 9]
+        expected = [-4, 0, 1, 2, 3, 5, 5, 7, 9]
         self.assertEqual(quick_sort(array), expected)
 
     def test2(self):
@@ -52,6 +71,28 @@ class TestCase(unittest.TestCase):
         array = []
         expected = []
         self.assertEqual(quick_sort(array), expected)
+
+
+class TestCase2(unittest.TestCase):
+    def test(self):
+        array = [5, 7, 1, 9, 5, -4, 3, 0, 2]
+        expected = [-4, 0, 1, 2, 3, 5, 5, 7, 9]
+        self.assertEqual(quick_sort2(array), expected)
+
+    def test2(self):
+        array = [1, 2, 3]
+        expected = [1, 2, 3]
+        self.assertEqual(quick_sort2(array), expected)
+
+    def test3(self):
+        array = [1, ]
+        expected = [1, ]
+        self.assertEqual(quick_sort2(array), expected)
+
+    def test4(self):
+        array = []
+        expected = []
+        self.assertEqual(quick_sort2(array), expected)
 
 
 unittest.main()
--- algorithms/sorting/selection_sort.py
+++ algorithms/sorting/selection_sort.py
@@ -1,26 +1,11 @@
 # coding: utf-8
 """
-Selection Sort divides the input list into two parts:
-a sorted list which is initially empty,
-and a list of the remaining unsorted items.
+Selection Sort
+https://en.wikipedia.org/wiki/Selection_sort
 
-Both two lists could be either two independent lists
-or the same array which mentally divided into two sublists.
-
-The algorithm proceeds by
-finding the smallest item in the unsorted list,
-moving it to the sorted list,
-then repeating in the remaining unsorted list.
-
-Or
-
-The algorithm proceeds by
-finding the smallest item in the unsorted sublist,
-swapping it with the leftmost item in the unsorted sublist,
-the swapped leftmost item belongs to the sorted sublist,
-then repeating in the remaining unsorted sublist.
-
-Complexity: O(n^2)
+Worst-case Complexity: О(n^2) comparisons, О(n) swaps
+Best-case Complexity: О(n^2) comparisons, О(n) swaps
+Average Complexity: О(n^2) comparisons, О(n) swaps
 """
 import unittest
 
@@ -38,9 +23,11 @@ def selection_sort(arr):
                 min_index = i
         return min_index
 
+    # The length of arr is reduced by 1 every loop because of arr.pop()
     for _ in range(len(arr)):
-        # the length of arr is reduced by 1 every loop because of arr.pop()
+        # Find the smallest item in the unsorted list,
         index = find_min_index(arr)
+        # Move it to the sorted list
         sorted_arr.append(arr.pop(index))
 
     return sorted_arr
@@ -49,13 +36,13 @@ def selection_sort(arr):
 def selection_sort2(arr):
     length = len(arr)
     for i in range(length - 1):
-        # find the smallest item in the unsorted sublist
+        # Find the smallest item in the unsorted sublist
         min_index = i
         for j in range(i + 1, length):
             if arr[j] < arr[min_index]:
                 min_index = j
 
-        # swap the smallest item with the leftmost item of the unsorted sublist
+        # Swap the smallest item with the leftmost item of the unsorted sublist
         if min_index != i:
             # arr[i] belongs to the sorted sublist
             arr[i], arr[min_index] = arr[min_index], arr[i]
@@ -66,7 +53,7 @@ def selection_sort2(arr):
 class TestCase(unittest.TestCase):
     def test(self):
         array = [5, 7, 1, 9, 5, -4, 3, 0, 2]
-        expected = [-4, 0, 1, 2 ,3, 5, 5, 7, 9]
+        expected = [-4, 0, 1, 2, 3, 5, 5, 7, 9]
         self.assertEqual(selection_sort(array), expected)
 
     def test2(self):
