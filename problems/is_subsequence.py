8cb866c9cff78367cb0cbfa8f03ec6415af21f55
Vinta Chen <vinta.chen@gmail.com>
2020-06-17 02:43:37 +0800

    refine

--- problems/is_subsequence.py
+++ problems/is_subsequence.py
@@ -10,15 +10,18 @@ class Solution:
         if not s:
             return True
 
+        s_len = len(s)
         hits = 0
         start = 0
         for s_c in s:
             for i, t_c in enumerate(t[start:]):
                 if s_c == t_c:
                     hits = hits + 1
-                    if hits == len(s):
+                    if hits == s_len:
                         return True
 
+                    # Found the matching character for `s_c`, so break to the next character of `s`
+                    # Start from the next index instead of t[0]
                     start = start + i + 1
                     break
 
