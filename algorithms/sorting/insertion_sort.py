3cdcc377350fafe282fe10f5452fd9e8ca57c6cb
Vinta Chen <vinta.chen@gmail.com>
2020-06-17 18:37:47 +0800

    refine

--- algorithms/sorting/insertion_sort.py
+++ algorithms/sorting/insertion_sort.py
@@ -15,7 +15,7 @@ def insertion_sort(arr):
     for i in range(1, len(arr)):
         # Iterate "the sorted part" backward, and find the correct position for arr[i]
         while (i > 0) and (arr[i] < arr[i - 1]):
-            # If we use for loop above, we will have to run `i` times for every unsorted element
+            # If we use for loop above, we will have to run `i` steps for every unsorted element
             # However, arr[i] > arr[i - 1] means arr[i] is already in the right position
             # No need to check remaining elements of the sorted part
             arr[i], arr[i - 1] = arr[i - 1], arr[i]
