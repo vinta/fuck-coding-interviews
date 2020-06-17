be99a20151fb3dee087221dc95b86fbc1fe051eb
Vinta Chen <vinta.chen@gmail.com>
2020-06-17 17:05:56 +0800

    fix naming

--- problems/is_subsequence.py
+++ problems/is_subsequence.py
@@ -11,18 +11,18 @@ class Solution:
             return True
 
         s_len = len(s)
-        hits = 0
-        start = 0
-        for s_c in s:
-            for i, t_c in enumerate(t[start:]):
-                if s_c == t_c:
-                    hits = hits + 1
-                    if hits == s_len:
+        char_hits = 0
+        t_start_index = 0
+        for s_char in s:
+            for i, t_char in enumerate(t[t_start_index:]):
+                if s_char == t_char:
+                    char_hits = char_hits + 1
+                    if char_hits == s_len:
                         return True
 
-                    # Found the matching character for `s_c`, so break to the next character of `s`
+                    # Found the matching character for `s_char`, so break to the next character of `s`
                     # Start from the next index instead of t[0]
-                    start = start + i + 1
+                    t_start_index = t_start_index + i + 1
                     break
 
         return False
