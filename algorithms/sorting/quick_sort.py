dc4bd332cff9fd073aa968ecf0ad7cfc84fd6721
Vinta Chen <vinta.chen@gmail.com>
2020-06-23 21:20:06 +0800

    fix Quick Sort

--- algorithms/sorting/quick_sort.py
+++ algorithms/sorting/quick_sort.py
@@ -7,11 +7,49 @@ Worst-case Complexity: O(n^2)
 Best-case Complexity: O(n * log(n))
 Average Complexity: O(n * log(n))
 """
-import random
 import unittest
 
 
 def quick_sort(arr):
+    def partition(arr, start, end):
+        pivot_index = start
+        pivot = arr[pivot_index]
+        left = pivot_index + 1
+        right = end
+
+        while True:
+            while left <= right and arr[left] <= pivot:
+                left = left + 1
+
+            while left <= right and arr[right] >= pivot:
+                right = right - 1
+
+            if left <= right:
+                arr[left], arr[right] = arr[right], arr[left]
+            else:
+                break
+
+        # if pivot_index != right:
+        arr[pivot_index], arr[right] = arr[right], arr[pivot_index]
+        pivot_index = right
+
+        return pivot_index
+
+    def quick_sort_range(arr, start, end):
+        # base case
+        if start >= end:
+            return arr
+
+        # recursive case
+        pivot_index = partition(arr, start, end)
+        quick_sort_range(arr, start, pivot_index - 1)
+        quick_sort_range(arr, pivot_index + 1, end)
+        return arr
+
+    return quick_sort_range(arr, 0, len(arr) - 1)
+
+
+def quick_sort2(arr):
     if len(arr) <= 1:
         return arr
     else:
@@ -33,24 +71,6 @@ def quick_sort(arr):
         return quick_sort(left_list) + [pivot, ] + quick_sort(right_list)
 
 
-def quick_sort2(arr):
-    if len(arr) <= 1:
-        return arr
-    else:
-        # Select a random item as the pivot
-        rand_index = random.randint(0, len(arr) - 1)
-        pivot = arr.pop(rand_index)
-
-        left_list = []
-        right_list = []
-        for item in arr:
-            if item <= pivot:
-                left_list.append(item)
-            else:
-                right_list.append(item)
-        return quick_sort(left_list) + [pivot, ] + quick_sort(right_list)
-
-
 class TestCase(unittest.TestCase):
     def test(self):
         array = [5, 7, 1, 9, 5, -4, 3, 0, 2]
@@ -58,8 +78,8 @@ class TestCase(unittest.TestCase):
         self.assertEqual(quick_sort(array), expected)
 
     def test2(self):
-        array = [1, 2, 3]
-        expected = [1, 2, 3]
+        array = []
+        expected = []
         self.assertEqual(quick_sort(array), expected)
 
     def test3(self):
@@ -68,8 +88,13 @@ class TestCase(unittest.TestCase):
         self.assertEqual(quick_sort(array), expected)
 
     def test4(self):
-        array = []
-        expected = []
+        array = [1, 2, 3]
+        expected = [1, 2, 3]
+        self.assertEqual(quick_sort(array), expected)
+
+    def test5(self):
+        array = [3, 2, 1]
+        expected = [1, 2, 3]
         self.assertEqual(quick_sort(array), expected)
 
 
@@ -80,8 +105,8 @@ class TestCase2(unittest.TestCase):
         self.assertEqual(quick_sort2(array), expected)
 
     def test2(self):
-        array = [1, 2, 3]
-        expected = [1, 2, 3]
+        array = []
+        expected = []
         self.assertEqual(quick_sort2(array), expected)
 
     def test3(self):
@@ -90,8 +115,13 @@ class TestCase2(unittest.TestCase):
         self.assertEqual(quick_sort2(array), expected)
 
     def test4(self):
-        array = []
-        expected = []
+        array = [1, 2, 3]
+        expected = [1, 2, 3]
+        self.assertEqual(quick_sort2(array), expected)
+
+    def test5(self):
+        array = [3, 2, 1]
+        expected = [1, 2, 3]
         self.assertEqual(quick_sort2(array), expected)
 
 
