013df96234349f9f05d98b1ec74253054389fa1b
Vinta Chen <vinta.chen@gmail.com>
2020-06-25 05:59:32 +0800

    refine Quick Sort

--- algorithms/sorting/quick_sort.py
+++ algorithms/sorting/quick_sort.py
@@ -10,62 +10,57 @@ Average Complexity: O(n * log(n))
 import unittest
 
 
+# https://en.wikipedia.org/wiki/Quicksort#Hoare_partition_scheme
 def quick_sort(arr):
     # Partition is to move all items that less than the pivot to the left side of the pivot,
     # and move all items that greater than the pivot to the right side of the pivot
     # As a result, the pivot is in the correct index,
     # but items in both sides of the pivot don't necessarily end up sorted
     def partition(arr, start, end):
-        # Select the first item as the pivot,
-        # So the left cursor starts from the next index of the pivot
-        pivot_index = start
-        pivot = arr[pivot_index]
-        left = start + 1
+        # Select the middle item as the pivot
+        pivot = arr[round((start + end) / 2)]
+        left = start
         right = end
-
         while True:
-            # If the current item is less than or equal to the pivot,
+            # If the current item is less than the pivot,
             # it's in the proper side (left side of the pivot),
             # then we move the left cursor right to the next element
-            while left <= right and arr[left] <= pivot:
-                # The left cursor stops at the item which is greater than the pivot
+            while arr[left] < pivot:
+                # The left cursor stops at the item which is greater than or equal to the pivot
                 left = left + 1
 
-            # If the current item is greater than or equal to the pivot,
-            # it's in the proper side (right side of the pivot),
-            # then we move the right cursor left to the next element
-            while left <= right and arr[right] >= pivot:
-                # The right cursor stops at the item which is less than the pivot
+            # Behave oppositely to the left cursor
+            while arr[right] > pivot:
                 right = right - 1
 
-            # We find items for both left and right cursor that are in the wrong side,
-            # so we swap them ("the leftmost item which greater than pivot" and "the rightmost item which less than the pivot"),
+            # We swap both items the left and right cursor found that are in the wrong side,
             # In the end, we move all items to their proper side of the pivot
-            if left <= right:
+            if left < right:
                 arr[left], arr[right] = arr[right], arr[left]
+                # Since we already swapped both items the left and right cursor pointed to
+                # Cursors move to the next index
+                left = left + 1
+                right = right - 1
             else:
-                break
-
-        # We swap the pivot with the right cursor,
-        # so the pivot ends up in the correct index
-        if pivot_index != right:
-            arr[pivot_index], arr[right] = arr[right], arr[pivot_index]
-            pivot_index = right
-
-        return pivot_index
+                # If left >= right, we exit the loop
+                return right
 
     def quick_sort_range(arr, start, end):
-        if start < end:
-            pivot_index = partition(arr, start, end)
-            quick_sort_range(arr, start, pivot_index - 1)
-            quick_sort_range(arr, pivot_index + 1, end)
+        # Base case
+        # the array of size 0 or 1 which (start, end) is (0, -1) or (0, 0)
+        if start >= end:
+            return arr
 
+        # Recursive case
+        partition_index = partition(arr, start, end)
+        quick_sort_range(arr, start, partition_index - 1)
+        quick_sort_range(arr, partition_index + 1, end)
         return arr
 
     return quick_sort_range(arr, 0, len(arr) - 1)
 
 
-def quick_sort2(arr):
+def quick_sort_extra_space(arr):
     if len(arr) <= 1:
         return arr
     else:
@@ -76,7 +71,6 @@ def quick_sort2(arr):
         # According to whether they are less than or greater than the pivot
         left_list = []
         right_list = []
-
         # There might be duplicates of pivot
         pivot_list = []
         for item in arr:
@@ -88,61 +82,42 @@ def quick_sort2(arr):
                 pivot_list.append(item)
 
         # Sublists are then sorted recursively
-        return quick_sort2(left_list) + pivot_list + quick_sort2(right_list)
+        return quick_sort_extra_space(left_list) + pivot_list + quick_sort_extra_space(right_list)
 
 
 class TestCase(unittest.TestCase):
     def test(self):
-        array = [5, 7, 1, 9, 5, 5, -4, 3, 0, 2]
-        expected = [-4, 0, 1, 2, 3, 5, 5, 5, 7, 9]
-        self.assertEqual(quick_sort(array), expected)
-
-    def test2(self):
-        array = []
-        expected = []
-        self.assertEqual(quick_sort(array), expected)
-
-    def test3(self):
-        array = [1, ]
-        expected = [1, ]
-        self.assertEqual(quick_sort(array), expected)
-
-    def test4(self):
-        array = [1, 2, 3]
-        expected = [1, 2, 3]
-        self.assertEqual(quick_sort(array), expected)
-
-    def test5(self):
-        array = [3, 2, 1]
-        expected = [1, 2, 3]
-        self.assertEqual(quick_sort(array), expected)
+        test_lists = [
+            [5, 7, 1, 9, 5, 5, -4, 3, 0, 2],
+            [],
+            [1, ],
+            [1, 2],
+            [1, 2, 3],
+            [3, 2, 1],
+            [1, 2, 3, 4],
+            [4, 3, 2, 1],
+            [1, 1, 1, 1],
+        ]
+        for array in test_lists:
+            with self.subTest(array=array):
+                self.assertEqual(quick_sort(array.copy()), sorted(array))
 
 
 class TestCase2(unittest.TestCase):
     def test(self):
-        array = [5, 7, 1, 9, 5, -4, 3, 0, 2]
-        expected = [-4, 0, 1, 2, 3, 5, 5, 7, 9]
-        self.assertEqual(quick_sort2(array), expected)
-
-    def test2(self):
-        array = []
-        expected = []
-        self.assertEqual(quick_sort2(array), expected)
-
-    def test3(self):
-        array = [1, ]
-        expected = [1, ]
-        self.assertEqual(quick_sort2(array), expected)
-
-    def test4(self):
-        array = [1, 2, 3]
-        expected = [1, 2, 3]
-        self.assertEqual(quick_sort2(array), expected)
-
-    def test5(self):
-        array = [3, 2, 1]
-        expected = [1, 2, 3]
-        self.assertEqual(quick_sort2(array), expected)
+        test_lists = [
+            [5, 7, 1, 9, 5, 5, -4, 3, 0, 2],
+            [],
+            [1, ],
+            [1, 2],
+            [1, 2, 3],
+            [3, 2, 1],
+            [1, 1, 1, 1],
+        ]
+        for array in test_lists:
+            with self.subTest(array=array):
+                self.assertEqual(quick_sort_extra_space(array.copy()), sorted(array))
 
 
-unittest.main()
+if __name__ == '__main__':
+    unittest.main()
