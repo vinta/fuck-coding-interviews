f852aacbffbcb0ee5a716c59b6c99c6ac6b69e0f
Vinta Chen <vinta.chen@gmail.com>
2020-06-24 17:31:19 +0800

    add comments

--- problems/valid_parentheses.py
+++ problems/valid_parentheses.py
@@ -19,17 +19,22 @@ class Solution:
         stack = []
 
         for char in s:
+            # If we find an opening brace, we push it onto the stack.
+            # Having it on the stack means we're waiting to close that particular brace.
             if char in ['(', '{', '[']:
                 stack.append(char)
+            # If we find a closing brace, we inspect the top element in the stack. We then analyze:
             elif char in [')', '}', ']']:
                 try:
-                    left = stack.pop()
+                    opening = stack.pop()
                 except IndexError:
                     return False
-                right = mapping.get(left)
-                if char != right:
+                closing = mapping.get(opening)
+                if char != closing:
                     return False
 
+        # If we make it to the end of the line and there's still something left on the stack,
+        # that means there’s an opening brace without a corresponding closing brace
         if stack:
             return False
 
