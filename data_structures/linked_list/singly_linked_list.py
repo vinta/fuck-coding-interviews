ab689e8e1220d55ac57dc46476f91110e6b8a671
Vinta Chen <vinta.chen@gmail.com>
2020-06-25 07:54:38 +0800

    update LinkedList

--- data_structures/linked_list/singly_linked_list.py
+++ data_structures/linked_list/singly_linked_list.py
@@ -9,8 +9,24 @@ class ListNode:
 
 
 class LinkedList:
-    def __init__(self, head):
+    def __init__(self, head=None):
         self.head = head
+
+    def __len__(self):
+        length = 0
+        current_node = self.head
+        while current_node:
+            length += 1
+            current_node = current_node.next
+
+        return length
+
+    def __iter__(self):
+        current_node = self.head
+        while current_node:
+            # TODO: Should we yield the node itself?
+            yield current_node.value
+            current_node = current_node.next
 
     def node_of_index(self, index):
         # TODO: Support negative indexes
@@ -43,14 +59,60 @@ class LinkedList:
         return None
 
     def insert_at_index(self, index, value):
-        pass
+        # TODO: Support negative indexes
+        if index < 0:
+            raise IndexError
+
+        new_node = ListNode(value)
+        if index == 0:
+            new_node.next = self.head
+            self.head = new_node
+        else:
+            current_node = self.head
+            current_index = 0
+            while current_node:
+                # We find the node that before the node at the index we are looking for
+                if current_index == index - 1:
+                    next_node = current_node.next
+                    current_node.next = new_node
+                    new_node.next = next_node
+                    return
+
+                current_node = current_node.next
+                current_index += 1
+
+            raise IndexError
 
     def delete_at_index(self, index):
-        pass
+        if index == 0:
+            if not self.head:
+                raise IndexError
+
+            self.head = self.head.next
+        else:
+            current_node = self.head
+            current_index = 0
+            while current_node:
+                # We find the node that before the node at the index we are looking for
+                if current_index == index - 1:
+                    deleted_node = current_node.next
+                    if not deleted_node:
+                        raise IndexError
+
+                    next_node = deleted_node.next
+                    current_node.next = next_node
+                    return
+
+                current_node = current_node.next
+                current_index += 1
+
+            raise IndexError
 
 
 class TestCase(unittest.TestCase):
     def setUp(self):
+        self.empty_linked_list = LinkedList()
+
         self.node_0 = ListNode('once')
         self.node_1 = ListNode('upon')
         self.node_2 = ListNode('a')
@@ -58,10 +120,20 @@ class TestCase(unittest.TestCase):
         self.node_0.next = self.node_1
         self.node_1.next = self.node_2
         self.node_2.next = self.node_3
-
         self.linked_list = LinkedList(self.node_0)
 
+    def test_len(self):
+        self.assertEqual(len(self.empty_linked_list), 0)
+        self.assertEqual(len(self.linked_list), 4)
+
+    def test_iter(self):
+        self.assertEqual(list(self.empty_linked_list), [])
+        self.assertEqual(list(self.linked_list), ['once', 'upon', 'a', 'time'])
+
     def test_value_of_index(self):
+        with self.assertRaises(IndexError):
+            self.empty_linked_list.value_of_index(0)
+
         self.assertEqual(self.linked_list.value_of_index(0), self.node_0.value)
         self.assertEqual(self.linked_list.value_of_index(1), self.node_1.value)
         self.assertEqual(self.linked_list.value_of_index(2), self.node_2.value)
@@ -77,11 +149,80 @@ class TestCase(unittest.TestCase):
             self.linked_list.value_of_index(100)
 
     def test_index_of_value(self):
+        self.assertEqual(self.empty_linked_list.index_of_value('NOT EXIST'), None)
+
         self.assertEqual(self.linked_list.index_of_value('once'), 0)
         self.assertEqual(self.linked_list.index_of_value('upon'), 1)
         self.assertEqual(self.linked_list.index_of_value('a'), 2)
         self.assertEqual(self.linked_list.index_of_value('time'), 3)
+
         self.assertEqual(self.linked_list.index_of_value('NOT EXIST'), None)
+
+    def test_insert_at_index(self):
+        self.empty_linked_list.insert_at_index(0, '0')
+        self.assertEqual(self.empty_linked_list.value_of_index(0), '0')
+
+        self.linked_list.insert_at_index(0, '0')
+        self.assertEqual(self.linked_list.value_of_index(0), '0')
+        self.assertEqual(self.linked_list.value_of_index(1), self.node_0.value)
+        self.assertEqual(self.linked_list.value_of_index(2), self.node_1.value)
+        self.assertEqual(self.linked_list.value_of_index(3), self.node_2.value)
+        self.assertEqual(self.linked_list.value_of_index(4), self.node_3.value)
+
+        self.linked_list.insert_at_index(2, '2')
+        self.assertEqual(self.linked_list.value_of_index(0), '0')
+        self.assertEqual(self.linked_list.value_of_index(1), self.node_0.value)
+        self.assertEqual(self.linked_list.value_of_index(2), '2')
+        self.assertEqual(self.linked_list.value_of_index(3), self.node_1.value)
+        self.assertEqual(self.linked_list.value_of_index(4), self.node_2.value)
+        self.assertEqual(self.linked_list.value_of_index(5), self.node_3.value)
+
+        self.linked_list.insert_at_index(5, '5')
+        self.assertEqual(self.linked_list.value_of_index(5), '5')
+        self.assertEqual(self.linked_list.value_of_index(6), self.node_3.value)
+
+        self.linked_list.insert_at_index(7, '7')
+        self.assertEqual(self.linked_list.value_of_index(7), '7')
+
+        self.assertEqual(len(self.linked_list), 8)
+
+        with self.assertRaises(IndexError):
+            self.linked_list.insert_at_index(9, '9')
+
+        with self.assertRaises(IndexError):
+            self.linked_list.insert_at_index(100, '100')
+
+        with self.assertRaises(IndexError):
+            self.linked_list.insert_at_index(-1, '-1')
+
+    def test_delete_at_index(self):
+        with self.assertRaises(IndexError):
+            self.empty_linked_list.delete_at_index(0)
+
+        self.linked_list.delete_at_index(0)
+        self.assertEqual(self.linked_list.value_of_index(0), self.node_1.value)
+        self.assertEqual(self.linked_list.value_of_index(1), self.node_2.value)
+        self.assertEqual(self.linked_list.value_of_index(2), self.node_3.value)
+
+        self.linked_list.delete_at_index(1)
+        self.assertEqual(self.linked_list.value_of_index(0), self.node_1.value)
+        self.assertEqual(self.linked_list.value_of_index(1), self.node_3.value)
+
+        self.assertEqual(len(self.linked_list), 2)
+
+        self.linked_list.delete_at_index(1)
+        self.linked_list.delete_at_index(0)
+
+        self.assertEqual(len(self.linked_list), 0)
+
+        with self.assertRaises(IndexError):
+            self.linked_list.delete_at_index(2)
+
+        with self.assertRaises(IndexError):
+            self.linked_list.delete_at_index(100)
+
+        with self.assertRaises(IndexError):
+            self.linked_list.delete_at_index(-1)
 
 
 if __name__ == '__main__':
