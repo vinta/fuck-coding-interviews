762a1a9554d3ca2a949a90dc23035dd4c9ff59cf
Vinta Chen <vinta.chen@gmail.com>
2020-06-26 23:36:57 +0800

    linked list cycle

---
+++ problems/linked_list_cycle.py
@@ -0,0 +1,25 @@
+# coding: utf-8
+"""
+https://leetcode.com/problems/linked-list-cycle/
+"""
+class ListNode:
+    def __init__(self, x):
+        self.val = x
+        self.next = None
+
+
+class Solution:
+    def hasCycle(self, head: ListNode) -> bool:
+        visted_ids = set()
+
+        node = head
+        while node:
+            node_id = id(node)
+            if node_id in visted_ids:
+                return True
+            else:
+                visted_ids.add(node_id)
+
+            node = node.next
+
+        return False
