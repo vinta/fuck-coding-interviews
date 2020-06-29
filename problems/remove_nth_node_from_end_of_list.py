77bdc504bc276c44007e10e34790cb32beaace5b
Vinta Chen <vinta.chen@gmail.com>
2020-06-29 17:14:25 +0800

    another stupid solution for add remove nth node from end of list

--- problems/remove_nth_node_from_end_of_list.py
+++ problems/remove_nth_node_from_end_of_list.py
@@ -35,9 +35,77 @@ class Solution:
         return head
 
 
+class Solution2:
+    def removeNthFromEnd(self, head: ListNode, n: int) -> ListNode:
+        # Calculate the length of the LinkedList in a full while loop
+        length = 0
+        node = head
+        while node:
+            length += 1
+            node = node.next
+
+        if length <= 1:
+            return None
+
+        # Convert the negative index to the 0-based index
+        deleted_index = length - n
+
+        if deleted_index == 0:
+            head = head.next
+            return head
+
+        # Start from the head, and find the previous node of the node we're going to delete
+        index = 0
+        node = head
+        while node:
+            if index == deleted_index - 1:
+                previous_node = node
+                deleted_node = node.next
+                previous_node.next = deleted_node.next
+                return head
+
+            index += 1
+            node = node.next
+
+        return head
+
+
 class TestCase(unittest.TestCase):
     def setUp(self):
         self.solution = Solution()
+
+    def test(self):
+        head = list_to_listnode([1, 2, 3, 4, 5])
+        n = 2
+        expected_head = self.solution.removeNthFromEnd(head, n)
+        expected = [1, 2, 3, 5]
+        self.assertEqual(listnode_to_list(expected_head), expected)
+
+    def test2(self):
+        head = list_to_listnode([1, 2, 3, 4, 5])
+        n = 1
+        expected_head = self.solution.removeNthFromEnd(head, n)
+        expected = [1, 2, 3, 4]
+        self.assertEqual(listnode_to_list(expected_head), expected)
+
+    def test3(self):
+        head = list_to_listnode([1, 2])
+        n = 2
+        expected_head = self.solution.removeNthFromEnd(head, n)
+        expected = [2]
+        self.assertEqual(listnode_to_list(expected_head), expected)
+
+    def test4(self):
+        head = list_to_listnode([1])
+        n = 1
+        expected_head = self.solution.removeNthFromEnd(head, n)
+        expected = []
+        self.assertEqual(listnode_to_list(expected_head), expected)
+
+
+class TestCase2(unittest.TestCase):
+    def setUp(self):
+        self.solution = Solution2()
 
     def test(self):
         head = list_to_listnode([1, 2, 3, 4, 5])
