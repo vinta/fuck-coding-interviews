a06cf73bb6f697e48af60aa0e024b2dc1dce5b1f
Vinta Chen <vinta.chen@gmail.com>
2020-07-12 18:35:31 +0800

    add search in a binary search tree

---
+++ problems/search_in_a_binary_search_tree.py
@@ -0,0 +1,47 @@
+# coding: utf-8
+"""
+https://leetcode.com/problems/search-in-a-binary-search-tree/
+"""
+
+
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+
+class Solution:
+    def searchBST(self, root: TreeNode, val: int) -> TreeNode:
+        if not root:
+            return root
+
+        def search_node(node, value):
+            node_val = node.val
+            if value == node_val:
+                return node
+            elif value < node_val:
+                if node.left:
+                    return search_node(node.left, value)
+            elif value > node_val:
+                if node.right:
+                    return search_node(node.right, value)
+            return None
+
+        return search_node(root, val)
+
+
+class Solution2:
+    def searchBST(self, root: TreeNode, val: int) -> TreeNode:
+        def search_node(node, value):
+            if not node:
+                return None
+            node_val = node.val
+            if value == node_val:
+                return node
+            elif value < node_val:
+                return search_node(node.left, value)
+            elif value > node_val:
+                return search_node(node.right, value)
+
+        return search_node(root, val)
