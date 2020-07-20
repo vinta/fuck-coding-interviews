c836b927c8fce31fb05aa78df8ff2e84f777acae
Vinta Chen <vinta.chen@gmail.com>
2020-07-21 01:57:51 +0800

    add check-completeness-of-a-binary-tree from LeetCode

---
+++ problems/check_completeness_of_a_binary_tree.py
@@ -0,0 +1,28 @@
+# coding: utf-8
+"""
+https://leetcode.com/problems/check-completeness-of-a-binary-tree/
+"""
+from collections import deque
+
+
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+
+class Solution:
+    def isCompleteTree(self, root: TreeNode) -> bool:
+        queue = deque([root, ])
+        has_none = False
+        while queue:
+            node = queue.popleft()
+            if node:
+                if has_none:
+                    return False
+                queue.extend([node.left, node.right])
+            else:
+                has_none = True
+
+        return True
---
+++ problems/tests/test_check_completeness_of_a_binary_tree.py
@@ -0,0 +1,28 @@
+# coding: utf-8
+import unittest
+
+from problems.check_completeness_of_a_binary_tree import Solution
+from problems.utils.leetcode import deserialize_tree_str
+
+
+class TestCase(unittest.TestCase):
+    def setUp(self):
+        self.solution = Solution()
+
+    def test(self):
+        test_data = [
+            {'input': '[1,2,3,4,5,6]', 'expected': True},
+            {'input': '[1,2,3,4,5,null,7]', 'expected': False},
+            {'input': '[1,2,3,5,null,7,8]', 'expected': False},
+        ]
+        for data in test_data:
+            tree_str = data['input']
+            expected = data['expected']
+            with self.subTest(tree_str=tree_str, expected=expected):
+                root = deserialize_tree_str(tree_str)
+                output = self.solution.isCompleteTree(root)
+                self.assertEqual(output, expected)
+
+
+if __name__ == '__main__':
+    unittest.main()
