12df24dc90ce8ec28fc7b5783da7f4556023d3db
Vinta Chen <vinta.chen@gmail.com>
2020-07-23 16:08:13 +0800

    clean

--- data_structures/trees/base_tree.py
+++ data_structures/trees/base_tree.py
@@ -19,8 +19,9 @@ class BaseNode(ABC):
 class BaseTree(ABC):
     NODE_CLASS = BaseNode
 
+    @abstractmethod
     def __init__(self):
-        self.root = None
+        ...
 
     @abstractmethod
     def __eq__(self, value):
@@ -52,44 +53,17 @@ class BaseTree(ABC):
         return not self.is_external(node)
 
     @abstractmethod
-    def parent(self, node):
-        ...
-
-    @abstractmethod
-    def children(self, node):
-        ...
-
-    @abstractmethod
     def traverse(self):
         ...
 
     @abstractmethod
     def height(self, node):
-        """
-        The height of a node is the number of edges on the longest path between the node and a descendant leaf.
-
-        If node is a leaf, then the height of node is 0.
-        Otherwise, the height of node is 1 + the maximum of heights of node's children.
-        """
-        if self.is_leaf(node):
-            return 0
-        return 1 + max(self.height(child) for child in self.children(node))
+        ...
 
     @abstractmethod
     def depth(self, node):
-        """
-        The depth of a node is the number of edges between the node and the root.
-
-        If node is the root, then the depth of node is 0.
-        Otherwise, the depth of node is 1 + the depth of node's parent.
-        """
-        if self.is_root(node):
-            return 0
-        return 1 + self.depth(self.parent(node))
+        ...
 
     @abstractmethod
     def level(self, node):
-        """
-        The level of a node is 1 + the depth of the node.
-        """
-        return 1 + self.depth(node)
+        ...
