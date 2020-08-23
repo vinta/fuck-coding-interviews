426c953d379ab90e5809f0d58d2e87a89eb4bd43
Vinta Chen <vinta.chen@gmail.com>
2020-08-24 00:16:06 +0800

    rename

--- data_structures/b_trees/b_plus_tree.py
+++ data_structures/b_trees/b_plus_tree.py
@@ -4,16 +4,17 @@ B+ Tree
 https://en.wikipedia.org/wiki/B%2B_tree
 """
 import bisect
+import math
 
 
-class Node:
-    __slots__ = ['tree', 'parent', 'keys', 'pointers']
+class BPlusTreeNode:
+    __slots__ = ['tree', 'parent', 'keys', 'values']
 
     def __init__(self, tree, parent=None):
         self.tree = tree
         self.parent = parent
         self.keys = []
-        self.pointers = []
+        self.values = []
 
     def __repr__(self):
         return f'{self.__class__.__name__}({self.keys})'
@@ -24,73 +25,85 @@ class Node:
     def is_root(self):
         return self.parent is None
 
+    def is_leaf(self):
+        return isinstance(self, BPlusTreeLeafNode)
+
     def is_overflow(self):
         return len(self.keys) > self.tree.order - 1
 
     def split(self):
         if self.is_root():
-            new_root = Node(tree=self.tree)
+            new_root = BPlusTreeNode(tree=self.tree)
             self.tree.root = new_root
             self.parent = new_root
-            self.parent.pointers = [self, ]
+            self.parent.values = [self, ]
 
-        median_index = (0 + len(self.keys) - 1) // 2
+        median_index = self.tree.order // 2
         median_key = self.keys[median_index]
 
-        new_right = Node(tree=self.tree, parent=self.parent)
-        new_right.keys = self.keys[median_index + 1:]
-        new_right.pointers = self.pointers[median_index + 1:]
-        for child in new_right.pointers:
-            child.parent = new_right
+        if self.is_leaf():
+            new_right = BPlusTreeLeafNode(tree=self.tree, parent=self.parent)
+            new_right.keys = self.keys[median_index:]
+            new_right.values = self.values[median_index:]  # values are nodes.
+            new_right.next = self.next
 
-        self.keys = self.keys[:median_index]
-        self.pointers = self.pointers[:median_index + 1]  # NOTE: Here is "median_index + 1" instead of "median_index".
+            self.keys = self.keys[:median_index]
+            self.values = self.values[:median_index]
+            self.next = new_right
+        else:
+            new_right = BPlusTreeNode(tree=self.tree, parent=self.parent)
+            new_right.keys = self.keys[median_index + 1:]
+            new_right.values = self.values[median_index + 1:]  # values are data.
+            for child in new_right.values:
+                child.parent = new_right
+
+            self.keys = self.keys[:median_index]
+            self.values = self.values[:median_index + 1]  # NOTE: Here is "median_index + 1" instead of "median_index".
 
         child_index = bisect.bisect_left(self.parent.keys, median_key)
         self.parent.keys.insert(child_index, median_key)
-        self.parent.pointers.insert(child_index + 1, new_right)
+        self.parent.values.insert(child_index + 1, new_right)
         if self.parent.is_overflow():
             self.parent.split()
 
     def insert(self, key, value):
         index = bisect.bisect_left(self.keys, key)
         self.keys.insert(index, key)
-        self.pointers.insert(index, value)
+        self.values.insert(index, value)
         if self.is_overflow():
             self.split()
 
+    def check_validation(self):
+        if len(self.tree):
+            assert self.keys
+            assert self.keys == sorted(set(self.keys))
 
-class LeafNode(Node):
-    __slots__ = ['tree', 'parent', 'keys', 'pointers', 'next']
+        num_pointers = len(self.pointers)
+        assert num_pointers <= self.tree.order, num_pointers
+
+        if not self.is_leaf() and not self.is_root():  # Internal nodes.
+            assert num_pointers >= math.ceil(self.tree.order / 2), num_pointers
+
+        if self.is_root() and not self.is_leaf():
+            assert num_pointers >= 2, num_pointers
+
+        if self.is_leaf():
+            for value in self.values:
+                assert not isinstance(value, (BPlusTreeNode, BPlusTreeLeafNode))
+        else:
+            assert num_pointers - 1 == len(self.keys)
+
+            for child in self.values:
+                assert child.parent == self
+                child.check_validation()
+
+
+class BPlusTreeLeafNode(BPlusTreeNode):
+    __slots__ = ['tree', 'parent', 'keys', 'values', 'next']
 
     def __init__(self, tree, parent=None):
         super().__init__(tree, parent=parent)
         self.next = None
-
-    def split(self):
-        if self.is_root():
-            new_root = Node(tree=self.tree)
-            self.tree.root = new_root
-            self.parent = new_root
-            self.parent.pointers = [self, ]
-
-        median_index = (0 + len(self.keys) - 1) // 2
-        median_key = self.keys[median_index]
-
-        new_right = LeafNode(tree=self.tree, parent=self.parent)
-        new_right.keys = self.keys[median_index:]
-        new_right.pointers = self.pointers[median_index:]
-        new_right.next = self.next
-
-        self.keys = self.keys[:median_index]
-        self.pointers = self.pointers[:median_index]
-        self.next = new_right
-
-        child_index = bisect.bisect_left(self.parent.keys, median_key)
-        self.parent.keys.insert(child_index, median_key)
-        self.parent.pointers.insert(child_index + 1, new_right)
-        if self.parent.is_overflow():
-            self.parent.split()
 
 
 class BPlusTree:
@@ -100,13 +113,13 @@ class BPlusTree:
         if order < 3:
             raise ValueError('order must be greater than or equal to 3')
 
-        self._size = 0
+        self.size = 0
         self.order = order
-        self.root = LeafNode(tree=self)
+        self.root = BPlusTreeLeafNode(tree=self)
         self.head = self.root
 
     def __len__(self):
-        return self._size
+        return self.size
 
     def __iter__(self):
         leaf_node = self.head
@@ -115,17 +128,17 @@ class BPlusTree:
             leaf_node = leaf_node.next
 
     def search_node(self, key, node):
-        if isinstance(node, LeafNode):
+        if isinstance(node, BPlusTreeLeafNode):
             return node
 
         index = bisect.bisect_right(node.keys, key)
-        return self.search_node(key, node.pointers[index])
+        return self.search_node(key, node.values[index])
 
     def get(self, key):
         node = self.search_node(key, self.root)
         index = bisect.bisect_left(node.keys, key)
         if index < len(node.keys) and node.keys[index] == key:
-            return node.pointers[index]
+            return node.values[index]
 
         raise KeyError
 
@@ -136,4 +149,4 @@ class BPlusTree:
             raise KeyError('Duplicate key')
 
         node.insert(key, value)
-        self._size += 1
+        self.size += 1
--- data_structures/b_trees/tests/test_b_plus_tree.py
+++ data_structures/b_trees/tests/test_b_plus_tree.py
@@ -26,12 +26,15 @@ class BPlusTreeTest(unittest.TestCase):
         for key in self.keys:
             self.assertEqual(self.b_plus_tree.get(key), f'{key}')
 
+        with self.assertRaises(KeyError):
+            self.b_plus_tree.get(-1)
+
     def test_insert(self):
         b_plus_tree = BPlusTree(order=3)
 
         b_plus_tree.insert(key=86, value='86')
         self.assertEqual(b_plus_tree.root.keys, [86])
-        self.assertEqual(b_plus_tree.root.pointers, ['86'])
+        self.assertEqual(b_plus_tree.root.values, ['86'])
         self.assertEqual(list(b_plus_tree), [86])
 
         with self.assertRaises(KeyError):
@@ -39,42 +42,42 @@ class BPlusTreeTest(unittest.TestCase):
 
         b_plus_tree.insert(key=99, value='99')
         self.assertEqual(b_plus_tree.root.keys, [86, 99])
-        self.assertEqual(b_plus_tree.root.pointers, ['86', '99'])
+        self.assertEqual(b_plus_tree.root.values, ['86', '99'])
         self.assertEqual(list(b_plus_tree), [86, 99])
 
         b_plus_tree.insert(key=5, value='5')
         self.assertEqual(b_plus_tree.root.keys, [86])
-        self.assertEqual([leaf.keys for leaf in b_plus_tree.root.pointers], [[5], [86, 99]])
-        self.assertEqual([leaf.pointers for leaf in b_plus_tree.root.pointers], [['5'], ['86', '99']])
+        self.assertEqual([leaf.keys for leaf in b_plus_tree.root.values], [[5], [86, 99]])
+        self.assertEqual([leaf.values for leaf in b_plus_tree.root.values], [['5'], ['86', '99']])
         self.assertEqual(list(b_plus_tree), [5, 86, 99])
 
         b_plus_tree.insert(key=17, value='17')
         self.assertEqual(b_plus_tree.root.keys, [86])
-        self.assertEqual([leaf.keys for leaf in b_plus_tree.root.pointers], [[5, 17], [86, 99]])
-        self.assertEqual([leaf.pointers for leaf in b_plus_tree.root.pointers], [['5', '17'], ['86', '99']])
+        self.assertEqual([leaf.keys for leaf in b_plus_tree.root.values], [[5, 17], [86, 99]])
+        self.assertEqual([leaf.values for leaf in b_plus_tree.root.values], [['5', '17'], ['86', '99']])
         self.assertEqual(list(b_plus_tree), [5, 17, 86, 99])
 
         b_plus_tree.insert(key=24, value='24')
         self.assertEqual(b_plus_tree.root.keys, [17, 86])
-        self.assertEqual(b_plus_tree.root.pointers[0].keys, [5])
-        self.assertEqual(b_plus_tree.root.pointers[1].keys, [17, 24])
-        self.assertEqual(b_plus_tree.root.pointers[2].keys, [86, 99])
+        self.assertEqual(b_plus_tree.root.values[0].keys, [5])
+        self.assertEqual(b_plus_tree.root.values[1].keys, [17, 24])
+        self.assertEqual(b_plus_tree.root.values[2].keys, [86, 99])
         self.assertEqual(list(b_plus_tree), [5, 17, 24, 86, 99])
 
         b_plus_tree.insert(key=53, value='53')
         self.assertEqual(b_plus_tree.root.keys, [24])
-        self.assertEqual(b_plus_tree.root.pointers[0].keys, [17])
-        self.assertEqual(b_plus_tree.root.pointers[1].keys, [86])
-        self.assertEqual([leaf_node.keys for leaf_node in b_plus_tree.root.pointers[0].pointers], [[5], [17]])
-        self.assertEqual([leaf_node.keys for leaf_node in b_plus_tree.root.pointers[1].pointers], [[24, 53], [86, 99]])
+        self.assertEqual(b_plus_tree.root.values[0].keys, [17])
+        self.assertEqual(b_plus_tree.root.values[1].keys, [86])
+        self.assertEqual([leaf_node.keys for leaf_node in b_plus_tree.root.values[0].values], [[5], [17]])
+        self.assertEqual([leaf_node.keys for leaf_node in b_plus_tree.root.values[1].values], [[24, 53], [86, 99]])
         self.assertEqual(list(b_plus_tree), [5, 17, 24, 53, 86, 99])
 
         b_plus_tree.insert(key=31, value='31')
         self.assertEqual(b_plus_tree.root.keys, [24])
-        self.assertEqual(b_plus_tree.root.pointers[0].keys, [17])
-        self.assertEqual(b_plus_tree.root.pointers[1].keys, [31, 86])
-        self.assertEqual([leaf_node.keys for leaf_node in b_plus_tree.root.pointers[0].pointers], [[5], [17]])
-        self.assertEqual([leaf_node.keys for leaf_node in b_plus_tree.root.pointers[1].pointers], [[24], [31, 53], [86, 99]])
+        self.assertEqual(b_plus_tree.root.values[0].keys, [17])
+        self.assertEqual(b_plus_tree.root.values[1].keys, [31, 86])
+        self.assertEqual([leaf_node.keys for leaf_node in b_plus_tree.root.values[0].values], [[5], [17]])
+        self.assertEqual([leaf_node.keys for leaf_node in b_plus_tree.root.values[1].values], [[24], [31, 53], [86, 99]])
         self.assertEqual(list(b_plus_tree), [5, 17, 24, 31, 53, 86, 99])
 
         with self.assertRaises(KeyError):
