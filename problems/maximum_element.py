6db622f80a7a1a990aaee62a02919569bfd108a0
Vinta Chen <vinta.chen@gmail.com>
2020-09-18 17:57:57 +0800

    add Maximum Element from HackerRank

---
+++ problems/maximum_element.py
@@ -0,0 +1,30 @@
+# coding: utf-8
+"""
+https://www.hackerrank.com/challenges/maximum-element/problem
+"""
+_ = input()  # The total lines.
+
+stack = []
+track_stack = []
+while True:
+    try:
+        line = input().split()
+    except EOFError:
+        break
+
+    if line[0] == '1':  # Push the element x into the stack.
+        num = int(line[1])
+        stack.append(num)
+        try:
+            current_max = track_stack[-1]
+        except IndexError:
+            track_stack.append(num)
+        else:
+            if num > current_max:
+                current_max = num
+            track_stack.append(current_max)
+    elif line[0] == '2':  # Delete the element present at the top of the stack.
+        stack.pop()
+        track_stack.pop()
+    elif line[0] == '3':  # Print the maximum element in the stack.
+        print(track_stack[-1])
