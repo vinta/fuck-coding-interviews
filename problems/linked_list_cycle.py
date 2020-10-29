fd551ae41dc1fad15e8be20b29fde9ceea641390
Vinta Chen <vinta.chen@gmail.com>
2020-10-30 01:14:14 +0800

    add another solution for Linked List Cycle

--- problems/linked_list_cycle.py
+++ problems/linked_list_cycle.py
@@ -25,3 +25,19 @@ class Solution:
             node = node.next
 
         return False
+
+
+class Solution2:
+    def hasCycle(self, head: ListNode) -> bool:
+        if not head:
+            return False
+
+        slow = head
+        fast = head
+        while fast and fast.next:
+            slow = slow.next
+            fast = fast.next.next
+            if slow == fast:
+                return True
+
+        return False
